dafa7b41ed18b370d7272f26d0f8a7140ac5cb11
ABowlOfMilk <119057420+ABowlOfMilk@users.noreply.github.com>
2022-11-25 22:55:22 -0500

    Update Game.py

--- VIsual Studio Code/games and shits/Game.py
+++ VIsual Studio Code/games and shits/Game.py
@@ -13,15 +13,19 @@ Text = pygame.font.Font("Fonts/Pixel Millennium.ttf", 25)
 #Background
 sky_surface = pygame.image.load("graphics/Sky.png").convert()
 ground = pygame.image.load("graphics/ground.png").convert()
-Surface_Text = Text.render("My Game is Trash", False, "Black")
+
+#SCORE
+Score_surf = Text.render("My Game", False, (64,64,64))
+Score_rect = Score_surf.get_rect(center = (400,50))
 
 #BAD BAD
 BadGuy = pygame.image.load("graphics/BAD_BAD.png").convert_alpha()
-BadGuy_centerX = 600
+BadGuy_rect = BadGuy.get_rect(bottomright = (600,300))
 
 #Player
 player = pygame.image.load("graphics/Player.png").convert_alpha()
 player_rect = player.get_rect(midbottom = (80,300))
+player_gravity = 0
 
 #Shit that runs
 while True:
@@ -30,18 +34,44 @@ while True:
             pygame.quit()
             exit()
 
+
+        #onMousePress
+        if event.type == pygame.MOUSEBUTTONDOWN:
+           if player_rect.collidepoint(event.pos) and player_rect.bottom >=300:
+                player_gravity = -20
+
+        #onKeyPress
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE and player_rect.bottom >=300:
+                player_gravity = -20
+
+
     #Background Stuffs
     screen.blit(sky_surface,(0,0))
     screen.blit(ground,(0,300))
-    screen.blit(Surface_Text,(250,50))
+
+    #score
+    pygame.draw.rect(screen,"#c0e8ec",Score_rect)
+    pygame.draw.rect(screen,"#c0e8ec",Score_rect,10)
+    screen.blit(Score_surf,Score_rect)
+
     
     #BadGuy Stuffs
-    screen.blit(BadGuy,(BadGuy_centerX,225))
-    BadGuy_centerX -= 4
-    if BadGuy_centerX < -100: BadGuy_centerX = 800
+    screen.blit(BadGuy,BadGuy_rect)
+    BadGuy_rect.x -=4
+    if BadGuy_rect.right <= 0: BadGuy_rect.left = 800
     
     #Player Stuffs
+    player_gravity +=1
+    player_rect.y += player_gravity
+    if player_rect.bottom >=300: 
+        player_rect.bottom = 300
     screen.blit(player,player_rect)
+
+    #collision
+    if BadGuy_rect.colliderect(player_rect):
+        pygame.quit()
+        exit()
 
     #other things that need to be there for the shit to run good
     pygame.display.update()
